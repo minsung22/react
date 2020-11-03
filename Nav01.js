8f1fe631eb7b74506638966374386242c94b83ea
minsung22 <minsungg222@gmail.com>
2020-11-03 16:47:46 +0900

    2

--- Nav01.js
+++
@@ -1,9 +0,0 @@
-function Min1(){
-    return(
-        <div>
-            민성이1
-        </div>
-    );
-}
-
-export default Min1;
--- Nav02.js
+++
@@ -1,9 +0,0 @@
-function Min2(){
-    return(
-        <div>
-            민성이2
-        </div>
-    );
-}
-
-export default Min2;
